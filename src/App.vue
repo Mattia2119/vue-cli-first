69daea0cf51727dea9c5ec682863497780a92890
Mattia Bardino <Mattia.errol@gmail.com>
2022-02-21 15:41:22 +0100

    ,

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PrimoComponente/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PrimoComponente from './components/PrimoComponente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PrimoComponente
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PrimoComponente.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>La mia prima app con Vue CLI!</h2>
+</template>
+
+<script>
+export default {
+  name: 'PrimoComponente',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
